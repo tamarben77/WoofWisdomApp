8851adbc1ecbc3cc2b13776da832b67faed99d16
Tamar Ben Hillel <tamar.benhillel@cognyte.com>
2023-05-08 20:40:56 +0300

    @ComponentScan(basePackages = {"WoofWisdom", "HandleBreedRequests"}) added, its handle the option of rest controller

--- Woof-Wisdom/Breeds/src/main/java/HandleBreedRequests/BreedController.java
+++ Woof-Wisdom/Breeds/src/main/java/HandleBreedRequests/BreedController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/dogBreed")
--- Woof-Wisdom/Server/src/main/java/WoofWisdom/WoofWisdomApplication.java
+++ Woof-Wisdom/Server/src/main/java/WoofWisdom/WoofWisdomApplication.java
@@ -2,8 +2,10 @@ package WoofWisdom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"WoofWisdom", "HandleBreedRequests"})
 public class WoofWisdomApplication {
 
 	public static void main(String[] args) {
