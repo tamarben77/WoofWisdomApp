0a9b03be8eea215802592e3a803ca8b1d656d5fe
neta-vega <75238818+neta-vega@users.noreply.github.com>
2023-08-11 23:33:25 +0300

    tested a new design for main screen - current version in test_activity2 (ui only). also added a layout for round buttons - called "button_background.xml"

---
+++ WoofWisdomApplication/app/src/main/java/com/example/woofwisdomapplication/testActivity2.java
@@ -0,0 +1,14 @@
+package com.example.woofwisdomapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class testActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test2);
+    }
+}
