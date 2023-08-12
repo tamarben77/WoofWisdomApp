e5c41704e4dc160c598b76f412dea6d3060d6a85
Tamar Ben Hillel <tamar.benhillel@cognyte.com>
2023-08-12 13:40:22 +0300

    Refactored login error handling and enhanced UI feedback
    - Enhanced error handling in the login function.
    - Updated visibility settings for loginFeedback TextView.
    - Improved layout parameters to ensure error messages are visible.

--- WoofWisdomApplication/app/src/main/java/com/example/woofwisdomapplication/login.java
+++ WoofWisdomApplication/app/src/main/java/com/example/woofwisdomapplication/login.java
@@ -1,7 +1,6 @@
 package com.example.woofwisdomapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -10,6 +9,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.text.method.PasswordTransformationMethod;
 
@@ -31,88 +31,118 @@ public class login extends AppCompatActivity {
     private static final String IP = System.getProperty("IP");
     private static final String URL = "http://" + IP + ":8091/auth/signIn";
     private boolean isPasswordVisible = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_login);
+        setupUIElements();
+    }
 
+    private void setupUIElements() {
         EditText emailEditText = findViewById(R.id.editTextEmail);
         EditText passwordEditText = findViewById(R.id.editTextPassword);
         Button loginButton = findViewById(R.id.loginButton);
         Button signUpButton = findViewById(R.id.signUpButton);
 
-        Log.d("mubi",URL);
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String email = emailEditText.getText().toString();
-                String password = passwordEditText.getText().toString();
+        Log.d("mubi", URL);
 
-                Map<String, String> requestBody = new HashMap<>();
-                requestBody.put("email", email);
-                requestBody.put("password", password);
-                String jsonBody = new Gson().toJson(requestBody);
-                JSONObject jsonRequestBody = null;
-                try {
-                    jsonRequestBody = new JSONObject(jsonBody);
-                } catch (JSONException e) {
-                    throw new RuntimeException(e);
-                }
+        loginButton.setOnClickListener(v -> loginUser(emailEditText, passwordEditText));
 
-                JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, URL, jsonRequestBody,
-                        new Response.Listener<JSONObject>() {
-                            @Override
-                            public void onResponse(JSONObject response) {
-                                SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
-                                SharedPreferences.Editor editor = sharedPreferences.edit();
-                                try {
-                                    editor.putString("sessionID", response.getString("sessionID"));
-                                } catch (JSONException e) {
-                                    throw new RuntimeException(e);
-                                }
-                                editor.apply();
-                                Log.d("LoginActivity", "Starting MainActivity");
-                                Intent intent = new Intent(login.this, MainActivity.class);
-                                Log.d("login", "Starting Main Activity");
-                                startActivity(intent);
-                                Log.d("login", "Main Activity started");
-                                Toast.makeText(login.this, "Login successful", Toast.LENGTH_SHORT).show();
-                            }
-                        },
-                        new Response.ErrorListener() {
-                            @Override
-                            public void onErrorResponse(VolleyError error) {
-                                Log.d("login", "login Failed");
-                                Toast.makeText(login.this, "Login failed: " + error.getMessage(), Toast.LENGTH_SHORT).show();
-                            }
-                        });
-                int timeout = 60000;
-                request.setRetryPolicy(new DefaultRetryPolicy(timeout,
-                        DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
-                Volley.newRequestQueue(login.this).add(request);
-            }
-        });
         ImageView eyeIcon = findViewById(R.id.eyeIcon);
-        eyeIcon.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                isPasswordVisible = !isPasswordVisible;
-                if (isPasswordVisible) {
-                    passwordEditText.setTransformationMethod(null);
-                } else {
-                    passwordEditText.setTransformationMethod(new PasswordTransformationMethod());
-                }
-                passwordEditText.setSelection(passwordEditText.getText().length());
-            }
-        });
+        eyeIcon.setOnClickListener(v -> togglePasswordVisibility(passwordEditText));
 
-        signUpButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(login.this, signUp.class);
-                startActivity(intent);
-            }
+        signUpButton.setOnClickListener(v -> {
+            Intent intent = new Intent(login.this, signUp.class);
+            startActivity(intent);
         });
+    }
+
+    private void togglePasswordVisibility(EditText passwordEditText) {
+        isPasswordVisible = !isPasswordVisible;
+        if (isPasswordVisible) {
+            passwordEditText.setTransformationMethod(null);
+        } else {
+            passwordEditText.setTransformationMethod(new PasswordTransformationMethod());
+        }
+        passwordEditText.setSelection(passwordEditText.getText().length());
+    }
+
+    private void loginUser(EditText emailEditText, EditText passwordEditText) {
+        String email = emailEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+
+        Map<String, String> requestBody = new HashMap<>();
+        requestBody.put("email", email);
+        requestBody.put("password", password);
+
+        sendLoginRequest(requestBody);
+    }
+
+    private void sendLoginRequest(Map<String, String> requestBody) {
+        String jsonBody = new Gson().toJson(requestBody);
+        JSONObject jsonRequestBody;
+        try {
+            jsonRequestBody = new JSONObject(jsonBody);
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, URL, jsonRequestBody,
+                this::handleLoginResponse,
+                this::handleLoginError);
+
+        int timeout = 60000;
+        request.setRetryPolicy(new DefaultRetryPolicy(timeout,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        Volley.newRequestQueue(login.this).add(request);
+    }
+
+    private void handleLoginResponse(JSONObject response) {
+        SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        try {
+            editor.putString("sessionID", response.getString("sessionID"));
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+        editor.apply();
+
+        TextView loginFeedback = findViewById(R.id.loginFeedback);
+        loginFeedback.setVisibility(View.GONE); // Hide the feedback text
+
+        Intent intent = new Intent(login.this, MainActivity.class);
+        startActivity(intent);
+        Toast.makeText(login.this, "Login successful", Toast.LENGTH_SHORT).show();
+    }
+
+    private void handleLoginError(VolleyError error) {
+
+        TextView loginFeedback = findViewById(R.id.loginFeedback);
+        loginFeedback.setText("DEBUG: This is an error message!");
+        loginFeedback.setVisibility(View.VISIBLE); // Display the feedback text
+        loginFeedback.setText("Login failed. Please try again.");
+        Log.d("DEBUG", "Visibility: " + loginFeedback.getVisibility() + ", Text: " + loginFeedback.getText());
+        Toast.makeText(login.this, "Login failed. Please try again.", Toast.LENGTH_LONG).show();
+//        TextView loginFeedback = findViewById(R.id.loginFeedback);
+//        loginFeedback.setVisibility(View.VISIBLE); // Display the feedback text
+//
+//        if (error.networkResponse != null) {
+//            switch (error.networkResponse.statusCode) {
+//                case 401: // Unauthorized
+//                    loginFeedback.setText("Incorrect email or password.");
+//                    break;
+//                case 404: // Not Found
+//                    loginFeedback.setText("Email not registered.");
+//                    break;
+//                default:
+//                    loginFeedback.setText("Login failed: " + error.getMessage());
+//                    break;
+//            }
+//        } else {
+//            loginFeedback.setText("Login failed: " + error.getMessage());
+//        }
+//        Toast.makeText(login.this, "Login failed: " + error.getMessage(), Toast.LENGTH_LONG).show();
+
     }
 }
