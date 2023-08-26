51d3a438e54336e8fc170e4276d2bd49f75595a9
neta-vega <75238818+neta-vega@users.noreply.github.com>
2023-08-26 18:32:49 +0300

    logout implemented

--- WoofWisdomApplication/app/src/main/java/com/example/woofwisdomapplication/MainActivity.java
+++ WoofWisdomApplication/app/src/main/java/com/example/woofwisdomapplication/MainActivity.java
@@ -5,12 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.woofwisdomapplication.CacheManager.NetworkUtils;
+import com.example.woofwisdomapplication.DTO.UserObject;
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.time.LocalTime;
 
 public class MainActivity extends AppCompatActivity {
     public static String BASE_URL;
@@ -20,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        System.setProperty("IP", "192.168.1.23");
+        System.setProperty("IP", "192.168.1.17");
         BASE_URL ="http://" + System.getProperty("IP") + ":8091/";
 
         super.onCreate(savedInstanceState);
@@ -29,7 +39,11 @@ public class MainActivity extends AppCompatActivity {
         fillCache();
 
         welcomeTextView = findViewById(R.id.welcome_text);
-        UserUtils.displayWelcomeMessage(this, welcomeTextView);
+        SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+        String userJson = sharedPreferences.getString("user", null);
+
+        //UserUtils.displayWelcomeMessage(this, welcomeTextView);
+        Button logout = findViewById(R.id.logout_button);
 
         TextView buttonNearestVet = (TextView) findViewById(R.id.findVetButton);
         TextView buttonsuspiciousFoodBtn = (TextView) findViewById(R.id.suspiciousFoodButton);
@@ -78,6 +92,35 @@ public class MainActivity extends AppCompatActivity {
             );
             startActivity(secondActivityIntent);
         });
+
+        logout.setOnClickListener(view -> {
+            // Make an asynchronous request to logout URL
+            //new LogoutAsyncTask().execute();
+
+            // Clear sessionID in SharedPreferences
+            clearSessionID();
+            login.setVisibility(View.VISIBLE);
+            logout.setVisibility(View.GONE);
+            welcomeTextView.setText("© 2023 Woof Wisdom. All rights reserved.");
+            welcomeTextView.setTextSize(12);
+            welcomeTextView.setTextColor(Color.parseColor("#888888"));
+        });
+
+        if (userJson != null) {
+            // Deserialize the JSON string back into a UserObject
+            UserObject user = new Gson().fromJson(userJson, UserObject.class);
+
+            // Extract and set the first name and last name
+            welcomeTextView.setText("Good " + getTimeOfDay() + ", " + user.getFirstName());
+            // Set the text to bold
+            welcomeTextView.setTypeface(null, Typeface.BOLD);
+
+            // Set the text size to 16
+            welcomeTextView.setTextSize(24);
+            login.setVisibility(View.GONE);
+            logout.setVisibility(View.VISIBLE);
+            welcomeTextView.setTextColor(getResources().getColor(android.R.color.black));
+        }
     }
 
     private void fillCache(){
@@ -95,5 +138,53 @@ public class MainActivity extends AppCompatActivity {
                 // Data has been fetched and stored in the cache with the given key
             }
         });
+    }
+
+    public static String getTimeOfDay() {
+        LocalTime currentTime = LocalTime.now();
+
+        if (currentTime.isAfter(LocalTime.of(6, 0)) && currentTime.isBefore(LocalTime.of(12, 0))) {
+            return "Morning";
+        } else if (currentTime.isAfter(LocalTime.of(12, 0)) && currentTime.isBefore(LocalTime.of(16, 0))) {
+            return "Noon";
+        } else if (currentTime.isAfter(LocalTime.of(16, 0)) && currentTime.isBefore(LocalTime.of(19, 0))) {
+            return "Afternoon";
+        } else if (currentTime.isAfter(LocalTime.of(19, 0)) && currentTime.isBefore(LocalTime.of(23, 0))) {
+            return "Evening";
+        } else {
+            return "Night";
+        }
+    }
+
+    private class LogoutAsyncTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                // Create a URL object
+                URL url = new URL(BASE_URL + "auth/logout");
+
+                // Open a connection to the URL
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+                // Set up the connection
+                connection.setRequestMethod("GET");
+
+                // Get the response code
+                int responseCode = connection.getResponseCode();
+
+                // Close the connection
+                connection.disconnect();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
+
+    private void clearSessionID() {
+        SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("sessionID", "");
+        editor.apply();
     }
 }
